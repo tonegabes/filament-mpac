ca3392ca1540d9f2a34aea4e170e03cc209991d0
Tone Gabes <tonegabes@gmail.com>
2025-09-24 20:13:13 -0500

    feat: enhance ImagePicker component to include dark mode support and improve image handling logic

--- resources/views/filament/components/forms/image-picker.blade.php
+++ resources/views/filament/components/forms/image-picker.blade.php
@@ -2,6 +2,7 @@
     $id = $getId();
     $statePath = $getStatePath();
     $extraInputAttributeBag = $getExtraInputAttributeBag()->class(['opacity-0 absolute pointer-events-none']);
+    $images = $getImages();
 @endphp
 
 <x-dynamic-component
@@ -9,31 +10,35 @@
     :field="$field"
     class="fi-fo-image-picker"
 >
-    <div class="fi-fo-image-picker__items">
-        @foreach ($getImages() as $image)
-            @php
-                $inputId = "{$id}-{$image->name}";
-                $imageUrl = $image->getUrl();
-            @endphp
+    @if ($images->isEmpty())
+        <span class="fi-fo-image-picker__empty">Sem imagens na biblioteca.</span>
+    @else
+        <div class="fi-fo-image-picker__items">
+            @foreach ($images as $image)
+                @php
+                    $inputId = "{$id}-{$image->name}";
+                    $imageUrl = $image->getUrl();
+                @endphp
 
-            <div class="fi-fo-image-picker__item">
-                <input
-                    id="{{ $inputId }}"
-                    name="{{ $id }}"
-                    type="radio"
-                    value="{{ $image->getFilename() }}"
-                    wire:model="{{ $statePath }}"
-                    wire:loading.attr="disabled"
-                    {{ $extraInputAttributeBag }}
-                >
-                <x-filament::button
-                    :for="$inputId"
-                    tag="label"
-                    class="fi-fo-image-picker__button "
-                >
-                    <img src="{{ $imageUrl }}" alt="">
-                </x-filament::button>
-            </div>
-        @endforeach
-    </div>
+                <div class="fi-fo-image-picker__item">
+                    <input
+                        id="{{ $inputId }}"
+                        name="{{ $id }}"
+                        type="radio"
+                        value="{{ $image->getFilename() }}"
+                        wire:model="{{ $statePath }}"
+                        wire:loading.attr="disabled"
+                        {{ $extraInputAttributeBag }}
+                    >
+                    <x-filament::button
+                        :for="$inputId"
+                        tag="label"
+                        class="fi-fo-image-picker__button "
+                    >
+                        <img src="{{ $imageUrl }}" alt="">
+                    </x-filament::button>
+                </div>
+            @endforeach
+        </div>
+    @endif
 </x-dynamic-component>
