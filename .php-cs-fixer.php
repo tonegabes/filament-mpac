b4231e03a4733dd139bf35a2add3f95f7c0a4676
Tone Gabes <tonegabes@gmail.com>
2026-07-02 11:43:53 -0500

    chore: replace custom PHP-CS-Fixer configuration with shared standards from mp-coding-standards; update lint commands in composer.json to use new configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,37 +2,4 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
-use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
-
-$pint = json_decode((string) file_get_contents(__DIR__ . '/pint.json'), true);
-
-$finder = Finder::create()
-    ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/public',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
-    ])
-    ->exclude(...$pint['exclude'])
-    ->name('*.php')
-    ->notName('*.blade.php')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true)
-;
-
-return (new Config)
-    ->setFinder($finder)
-    ->setRules([
-        '@PSR12' => true,
-        ...$pint['rules'],
-    ])
-    ->setParallelConfig(
-        ParallelConfigFactory::detect()
-    )
-    ->setRiskyAllowed(true)
-;
+return require __DIR__ . '/vendor/tonegabes/mp-coding-standards/.php-cs-fixer.php';
