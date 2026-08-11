7793b41e39c66e25b890a273a57960369dd50625
Tone Gabes <tonegabes@gmail.com>
2026-08-11 11:20:29 -0500

    melhora form de usuario para ldap

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use App\Enums\AuthMode;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use ToneGabes\BetterOptions\Forms\Components\CheckboxCards;
 
@@ -25,15 +27,8 @@ class UserForm
                             ->maxLength(255)
                             ->required(),
 
-                        TextInput::make('email')
-                            ->email()
-                            ->maxLength(255)
-                            ->required(),
-
-                        TextInput::make('username')
-                            ->label('Nome de Usuário')
-                            ->maxLength(255)
-                            ->required(),
+                        self::getUsernameComponent(),
+                        self::getEmailComponent(),
 
                         ToggleButtons::make('is_active')
                             ->label('Ativo')
@@ -55,5 +50,45 @@ class UserForm
                     ]),
 
             ])->columns(2);
+    }
+
+    private static function getUsernameComponent(): TextInput
+    {
+        $usernameComponent = TextInput::make('username')
+            ->label('Nome de Usuário');
+
+        if (config('auth.mode') === AuthMode::Ldap->value) {
+            $usernameComponent
+                ->live()
+                ->debounce(500)
+                ->afterStateUpdated(function (Set $set, $state) {
+                    $set('email', $state . config('auth.ldap.email_domain'));
+                });
+        } else {
+            $usernameComponent
+                ->maxLength(255)
+                ->required();
+        }
+
+        return $usernameComponent;
+    }
+
+    private static function getEmailComponent(): TextInput
+    {
+        $emailComponent = TextInput::make('email');
+
+        if (config('auth.mode') === AuthMode::Ldap->value) {
+            $emailComponent
+                ->hint(config('auth.ldap.email_domain'))
+                ->helperText('O email será gerado automaticamente com base no nome de usuário.')
+                ->readonly();
+        } else {
+            $emailComponent
+                ->email()
+                ->maxLength(255)
+                ->required();
+        }
+
+        return $emailComponent;
     }
 }
